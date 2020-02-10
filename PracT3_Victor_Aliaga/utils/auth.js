fe5e8823fad0a1803c5bb361162655ca946b1e9f
valiagahernandez <48681152+valiagahernandez@users.noreply.github.com>
2020-02-10 13:20:58 +0100

    remove

--- PracT3_Victor_Aliaga/utils/auth.js
+++
@@ -1,9 +0,0 @@
-// middleware de autenticación que verificará si existe un usuario almacenado en sesión antes de dejar pasar
-
-let autenticacion = (req, res, next) => {
-    if (req.session && req.session.usuario)
-        return next();
-    else
-        res.redirect('/auth/login');
-};
-module.exports = autenticacion;
